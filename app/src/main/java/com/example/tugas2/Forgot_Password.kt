650d05999edcf0b6fa5e23027dcfdfef0eb487c4
fadirahendrawan07@gmail.com <lailiazahra2923>
2025-03-22 23:31:50 +0900

    Commit

--- app/src/main/java/com/example/tugas2/Forgot_Password.kt
+++ app/src/main/java/com/example/tugas2/Forgot_Password.kt
@@ -1,12 +1,17 @@
 package com.example.tugas2
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.tugas2.databinding.ActivityForgotPasswordBinding
+import com.example.tugas2.databinding.ActivityMainBinding
+import com.example.tugas2.databinding.ActivityRegisterBinding
 
 class Forgot_Password : AppCompatActivity() {
+    private lateinit var binding: ActivityForgotPasswordBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +20,13 @@ class Forgot_Password : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        binding = ActivityForgotPasswordBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button2.setOnClickListener {
+            val intentMain = Intent(this, MainActivity::class.java)
+            startActivity(intentMain)
         }
     }
 }
--- app/src/main/java/com/example/tugas2/register.kt
+++ app/src/main/java/com/example/tugas2/register.kt
@@ -1,12 +1,17 @@
 package com.example.tugas2
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.tugas2.databinding.ActivityMainBinding
+import com.example.tugas2.databinding.ActivityRegisterBinding
 
 class register : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +20,19 @@ class register : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button2.setOnClickListener {
+            val intentMain = Intent(this, MainActivity::class.java)
+            startActivity(intentMain)
+        }
+
+        binding.textView7.setOnClickListener {
+            val intentMain = Intent(this, MainActivity::class.java)
+            startActivity(intentMain)
         }
     }
 }
